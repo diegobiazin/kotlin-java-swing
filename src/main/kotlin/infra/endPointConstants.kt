94e181b4b7471c7bb565accbd2534a6d676ad529
Diego Biazin <diego_biazin@hotmail.com>
2018-08-27 22:05:32 -0300

    Criando pacotes auxiliares

---
+++ src/main/kotlin/infra/endPointConstants.kt
@@ -0,0 +1,12 @@
+package infra
+
+class EndPointConstants() {
+    object BASE {
+        val URL: String = "https://jsonplaceholder.typicode.com/"
+    }
+
+    object POST {
+        val ALL_POSTS: String = "post"
+        val SINGLE_POSTS: String = "post"
+    }
+}
